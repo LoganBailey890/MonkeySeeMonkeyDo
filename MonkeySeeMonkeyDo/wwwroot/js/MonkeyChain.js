1cbe65822422015377cd821f1650da516e84d4c4
mmargolin <mmargolin@student.neumont.edu>
2022-02-16 08:41:39 -0700

    Monke

---
+++ MonkeySeeMonkeyDo/wwwroot/js/MonkeyChain.js
@@ -0,0 +1,16 @@
+
+/*
+const LEFT_KEY = 37;
+const RIGHT_KEY = 39;
+const UP_KEY = 38;
+const DOWN_KEY = 40; const keyPressed = event.keyCode;
+*/
+
+var gameCanvas = document.getElementById("gameCanvas");
+
+var game = gameCanvas.getContext("2d");
+
+game.fillStyle = 'black';
+
+game.fillRect(0, 0, gameCanvas.clientWidth, gameCanvas.clientHeight);
+
