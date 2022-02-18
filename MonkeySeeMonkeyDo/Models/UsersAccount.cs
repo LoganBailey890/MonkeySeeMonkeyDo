dcd9b1f5a50debf009e05df5692ae1a39bbe021e
LoganBailey890 <lbailey@student.neumont.edu>
2022-02-18 09:45:02 -0700

    DataBase work

---
+++ MonkeySeeMonkeyDo/Models/UsersAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace MonkeySeeMonkeyDo.Models
+{
+    public class UsersAccount
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("UserName")]
+        [Required]
+        public string UserName { get; set; }
+
+        [BsonElement("Password")]
+        [Required]
+        public string Password { get; set; }
+
+        
+
+    }
+}
---
+++ MonkeySeeMonkeyDo/Services/UserService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MonkeySeeMonkeyDo.Models;
+
+namespace MonkeySeeMonkeyDo.Services
+{
+    public class UserService
+    {
+        private readonly IMongoCollection<UsersAccount> user;
+
+        public UserService()
+        {
+            //MongoClient client = new MongoClient()
+        }
+
+        public List<UsersAccount> Get()
+        {
+            return user.Find(user => true).ToList();
+        }
+
+        public UsersAccount Get(string id)
+        {
+            return user.Find(user => user.Id == id).FirstOrDefault();
+        }
+
+        public UsersAccount Create(UsersAccount account)
+        {
+            user.InsertOne(account);
+            return account;
+        }
+
+        public void Update(string id, UsersAccount userIn)
+        {
+            user.ReplaceOne(user => user.Id == id, userIn);
+            
+        }
+
+        public void Remove(UsersAccount usersAccount)
+        {
+            user.DeleteOne(user => user.Id == usersAccount.Id);
+        }
+
+        public void RemoveByID(string id)
+        {
+            user.DeleteOne(user => user.Id == id);
+        }
+    }
+}
