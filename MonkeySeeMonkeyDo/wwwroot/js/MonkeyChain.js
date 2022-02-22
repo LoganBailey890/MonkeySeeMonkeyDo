acbddce08861df57d247b33616a36594e52bf6b6
mmargolin <mmargolin@student.neumont.edu>
2022-02-22 09:44:16 -0700

    YO IT WORK LOL :D

--- MonkeySeeMonkeyDo/wwwroot/js/MonkeyChain.js
+++ MonkeySeeMonkeyDo/wwwroot/js/MonkeyChain.js
@@ -18,52 +18,64 @@ var monkeyImg = new Image();
 monkeyImg.src = "/images/Chain/Chain_Monkey.png";
 
 var bananaImg = new Image();
-monkeyImg.src = "/images/Chain/Chain_Banana.png";
+bananaImg.onload = imgLoaded;
+bananaImg.src = "/images/Chain/Chain_Banana.png";
 
 let dx = 0;
 
-let dy = 0;
+let dy = -20;
+
+let changingDirection = false;
+
+
 
 let chain =
     [
-        { x: 400, y: 580 }
+        { x: 400, y: 300 },
+        { x: 400, y: 320 },
+        { x: 400, y: 340 },
+        { x: 400, y: 360 }
     ]
 
-createFood();
+function imgLoaded() {
 
-drawFood();
+    console.log("| ALL IMAGES LOADED |")
 
-var stop = 0;
+    main();
 
-while (stop != 10) {
-    setTimeout(function onTick() {
-        drawFood()
-        advanceChain();
-        drawChain();
-        main();
-    }, 100)
-    stop++;
+    document.addEventListener("keydown", changeDirection);  
+
+    createFood();
+  
 }
 
-
-/*main();
-
 function main() {
+    
+
     setTimeout(function onTick() {
-        drawFood()
+        changingDirection = false;
+        clearCanvas();
         advanceChain();
-        drawChain();
-        main();
+        drawFood();
+        drawChain(); main();
     }, 100)
-}*/
+}
+
 
 
 function advanceChain() {
     const head = { x: chain[0].x + dx, y: chain[0].y + dy };
+
+  
+
+    chain.unshift(head); 
+
     const didEatFood = chain[0].x === foodX && chain[0].y === foodY;
     if (didEatFood) {
+        // Generate new food location
         createFood();
     } else {
+        // Remove the last part of snake body
         chain.pop();
     }
 }
@@ -73,17 +85,31 @@ function drawChain() {
 }
 
 function drawChainPart(chainPart) {
-
-
     console.log("Chain: " + chainPart.x + " | " + chainPart.y);
-
     game.drawImage(monkeyImg, chainPart.x, chainPart.y);
+}
 
+function createFood() {
+    foodX = randomTwenty(0, gameCanvas.width - 20);
+    foodY = randomTwenty(0, gameCanvas.height - 20);
 
+    console.log("Food: " + foodX + " | " + foodY);
+}
+
+function drawFood() {
+    game.drawImage(bananaImg, foodX, foodY);
+}
+
+function clearCanvas() {
+    //  Select the colour to fill the drawing
+    game.fillStyle = 'black';
+    //  Select the colour for the border of the canvas
   
 
-
-    
+    // Draw a "filled" rectangle to cover the entire canvas
+    game.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
+    // Draw a "border" around the entire canvas
+    game.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
 }
 
 function changeDirection(event) {
@@ -96,24 +122,27 @@ function changeDirection(event) {
     const keyPressed = event.keyCode;
 
     // Setting up direction consts
-    const goingUp = dy === -10;
-    const goingDown = dy === 10;
-    const goingRight = dx === 10;
-    const goingLeft = dx === -10;
+    const goingUp = dy === -20;
+    const goingDown = dy === 20;
+    const goingRight = dx === 20;
+    const goingLeft = dx === -20;
+
+    if (changingDirection) return;
+    changingDirection = true;
 
     // Setting up the keypress responses
     if (keyPressed === LEFT_KEY && !goingRight) {
-        dx = -10;
+        dx = -20;
         dy = 0;
     } if (keyPressed === UP_KEY && !goingDown) {
         dx = 0;
-        dy = -10;
+        dy = -20;
     } if (keyPressed === RIGHT_KEY && !goingLeft) {
-        dx = 10;
+        dx = 20;
         dy = 0;
     } if (keyPressed === DOWN_KEY && !goingDown) {
         dx = 0;
-        dy = 10;
+        dy = 20;
     }
 }
 
@@ -121,19 +150,3 @@ function randomTwenty(min, max) {
     return Math.round((Math.random() * (max - min) + min) / 20) * 20;
 }
 
-function createFood() {
-    foodX = randomTwenty(0, gameCanvas.width - 20);
-    foodY = randomTwenty(0, gameCanvas.height - 20);
-
-    console.log("Food: " + foodX + " | " + foodY);
-
-    chain.forEach(function isFoodOnChain(part) {
-        const foodIsOnChain = part.x == foodX && part.y == foodY
-        if (foodIsOnChain)
-            createFood();
-    });
-}
-
-function drawFood() {
-    game.drawImage(bananaImg, foodX, foodY);
-}
