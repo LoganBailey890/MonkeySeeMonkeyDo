67c7855c118db0458e66c0490bd20339ebadf340
amckinney-1 <amckinney@student.neumont.edu>
2022-02-24 09:38:59 -0700

    More MonkeySwing

--- MonkeySeeMonkeyDo/wwwroot/js/MonkeySwing.js
+++ MonkeySeeMonkeyDo/wwwroot/js/MonkeySwing.js
@@ -1,7 +1,7 @@
 ﻿var config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 700,
     physics: {
         default: 'arcade',
         arcade: {
@@ -19,67 +19,50 @@
 var game = new Phaser.Game(config);
 
 function preload() {
-    this.load.image('sky', 'images/swing/sky.png');
+    this.load.image('background', 'images/swing/forest.png');
     this.load.image('ground', 'images/swing/platform.png');
-    this.load.image('star', 'images/swing/star.png');
+    this.load.image('banana', 'images/swing/banana.png');
     this.load.image('bomb', 'images/swing/bomb.png');
-    this.load.spritesheet('dude',
-        'images/swing/dude.png',
-        { frameWidth: 32, frameHeight: 48 }
-    );
+    this.load.image('monkey','images/swing/monkey.png');
 }
 
+var target = new Phaser.Math.Vector2();
 var platforms;
 var score = 0;
 var scoreText;
+var text1;
+var text2;
 
 function create() {
-    this.add.image(400, 300, 'sky');
+    this.add.image(600, 400, 'background');
+
+    text1 = this.add.text(10, 10, '', { fill: '#00ff00' });
+    text2 = this.add.text(500, 10, '', { fill: '#00ff00' });
+
     scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });  
 
     platforms = this.physics.add.staticGroup();
 
-    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+    platforms.create(600, 700, 'ground').setScale(3).refreshBody();
 
     platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
     platforms.create(750, 220, 'ground');
 
-    player = this.physics.add.sprite(100, 450, 'dude');
+    player = this.physics.add.sprite(100, 450, 'monkey').setScale(0.15).refreshBody();
 
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
 
-    this.anims.create({
-        key: 'left',
-        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-        frameRate: 10,
-        repeat: -1
-    });
-
-    this.anims.create({
-        key: 'turn',
-        frames: [{ key: 'dude', frame: 4 }],
-        frameRate: 20
-    });
-
-    this.anims.create({
-        key: 'right',
-        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-        frameRate: 10,
-        repeat: -1
-    });
-
     stars = this.physics.add.group({
-        key: 'star',
-        repeat: 11,
-        setXY: { x: 12, y: 0, stepX: 70 }
+        key: 'banana',
+        repeat: 16,
+        setXY: { x: 12, y: 0, stepX: 70 },
+        setScale: {x: 0.125, y: 0.125}
     });
 
     stars.children.iterate(function (child) {
-
-        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-
+        child.setBounceY(Phaser.Math.FloatBetween(0.3, 0.6));
     });
 
     bombs = this.physics.add.group();
@@ -91,28 +74,40 @@ function create() {
     this.physics.add.collider(stars, platforms);
     this.physics.add.overlap(player, stars, collectStar, null, this);
 
-    cursors = this.input.keyboard.createCursorKeys();
+    this.input.mouse.disableContextMenu();
+
+    this.input.on('pointerdown', function (pointer) {
+
+        target.x = pointer.x;
+        target.y = pointer.y;
+
+        // Move at 400 px/s:
+        this.physics.moveToObject(player, target, 400);
+
+    }, this);
 }
 
 function update() {
-    if (cursors.left.isDown) {
-        player.setVelocityX(-160);
+    var pointer = this.input.activePointer;
 
-        player.anims.play('left', true);
-    }
-    else if (cursors.right.isDown) {
-        player.setVelocityX(160);
+    if (pointer.isDown) {
+        this.physics.moveToObject(player, target, 400);
+    } 
 
-        player.anims.play('right', true);
-    }
-    else {
-        player.setVelocityX(0);
+    text1.setText([
+        'x: ' + pointer.worldX,
+        'y: ' + pointer.worldY,
+        'isDown: ' + pointer.isDown
+    ]);
 
-        player.anims.play('turn');
-    }
+    var distance = Phaser.Math.Distance.Between(player.x, player.y, target.x, target.y);
 
-    if (cursors.up.isDown && player.body.touching.down) {
-        player.setVelocityY(-330);
+    if (player.body.speed < 0) {
+        //  4 is our distance tolerance. The faster it moves, the more tolerance is required.
+        if (distance < 4) {
+            player.body.reset(target.x, target.y);
+            player.anims.stop();
+        }
     }
 }
 
