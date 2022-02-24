557c0fe5ae448e699ab43cb2a899327bd5a3f863
ickiie <nicolasries8@gmail.com>
2022-02-24 09:37:33 -0700

    more TTT stuff

--- MonkeySeeMonkeyDo/wwwroot/js/ttt.js
+++ MonkeySeeMonkeyDo/wwwroot/js/ttt.js
@@ -1,5 +1,9 @@
 ﻿var game;
 
+const NONE = 0;
+const PLAYER_X = 1;
+const PLAYER_O = 2;
+
 window.onload = function() {
     var config = {
         type: Phaser.Auto,
@@ -16,6 +20,8 @@ class BootScene extends Phaser.Scene {
     }
     preload() {
         this.load.image('blankSquare', '../Images/TTT/blankSquare.png')
+        this.load.image('PlX', '../Images/TTT/X.png')
+        this.load.image('PlO', '../Images/TTT/O.png')
     }
     create() {
         this.scene.start("PlayGameScene");
@@ -27,26 +33,67 @@ class PlayGameScene extends Phaser.Scene {
         super('PlayGameScene');
     }
     create() {
+        let self = this;
+        self.whoseTurn = PLAYER_X;
+
         let blankSquareSize = 200;
         let halfBlankSquareSize = 100;
-        for (let row = 0; row < 3; row++) {
-            let y = halfBlankSquareSize + (blankSquareSize * row) + (row * 10);
-            for (let col = 0; col < 3; col++) {
-                let x = halfBlankSquareSize + (blankSquareSize * col) + (col * 10);
-                this.add.image(x, y, 'blankSquare');
+        let nKey = 0;
+
+        for (let row = 1; row <= 3; row++) {
+            let y = halfBlankSquareSize + (blankSquareSize * (row - 1)) + ((row - 1) * 10);
+            for (let col = 1; col <= 3; col++) {
+                let x = halfBlankSquareSize + (blankSquareSize * (col - 1)) + ((col - 1) * 10);
+
+                let blank = this.add.image(x, y, 'blankSquare');
+                blank.myKey = nKey++;
+                blank.occupiedBy = NONE;
+                blank.setInteractive();
+                blank.on('pointerdown', this.handleClick);
+
+                let textVal = 'myKey: ' + blank.myKey.toString();
+                let label = this.add.text(x, y, textVal,
+                    { fontSize: '16px Arial', fill: '#000' });
+                label.setOrigin(0.5, 0.5);
+
+            
+                //this.add.image(x, y, 'blankSquare');
             }
         }
-/*
-        this.add.image(100, 100, 'blankSquare');
-        this.add.image(310, 100, 'blankSquare');
-        this.add.image(520, 100, 'blankSquare');
+        IO = this.add.sprite(100, 100, 'PlO', 0);
+        IO.width = 100;
+        IO.height = 100;
+        this.playerO = IO;
+        IX = this.add.sprite(520, 520, 'PlX', 1);
+        IX.width = 100;
+        IX.height = 100;
+        this.playerX = IX;
+    }
 
-		this.add.image(100, 310, 'blankSquare');
-		this.add.image(310, 310, 'blankSquare');
-		this.add.image(520, 310, 'blankSquare');
+    handleClick(event) {
+        let owner = this.scene;
+        console.log("occupiedBy: " + this.occupiedBy); // this is our image object
+        let wt = owner.whoseTurn;
+        wt = (wt === PLAYER_X) ? PLAYER_O : PLAYER_X;
+        owner.whoseTurn = wt;
 
-		this.add.image(100, 520, 'blankSquare');
-		this.add.image(310, 520, 'blankSquare');
-		this.add.image(520, 520, 'blankSquare');*/
+        owner.whoseTurnIsIt();
+    }
+
+    whoseTurnIsIt() {
+        let x = this.game.config.width / 2;
+        let y = this.game.config.height / 2;
+        let t = (this.whoseTurn == PLAYER_X ? "PLAYER X" : "PLAYER O");
+
+        let label = this.add.text(x, y, t,
+            { fontSize: '72px Arial', fill: '#F00' });
+        label.setOrigin(0.5, 0.5);
+
+        this.tweens.add({
+            targets: label,
+            alpha: 0,
+            ease: 'Power1',
+            duration: 1000,
+        });
     }
 }
